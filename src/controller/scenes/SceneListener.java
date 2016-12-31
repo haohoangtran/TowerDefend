780915a53ee60bf6c17bc289354851fb875ed64d
DESKTOP-4AAPR0I\tranh <tranhaockpt@gmail.com>
2016-12-31 15:34:26 +0700

    edit loadScene

--- src/controller/scenes/SceneListener.java
+++
@@ -1,9 +0,0 @@
-package controller.scenes;
-
-/**
- * Created by DUC THANG on 12/28/2016.
- */
-public interface SceneListener {
-    void replaceScene(GameScene newScene, boolean addToBackStack);
-    void back();
-}
